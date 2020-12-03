26b4777dd3797f62d1c7feac0eeebad4becb0594
Kemoel <kemoel.amorim@gmail.com>
2020-12-03 19:43:35 -0300

    com js

---
+++ js/principal.js
@@ -0,0 +1,15 @@
+function salvador(){
+    document.getElementById("trocarimg").src="img/salvador.jpg";
+}
+function rio (){
+    document.getElementById("trocarimg").src="img/rio.jpg";
+}
+function blumenau (){
+    document.getElementById("trocarimg").src="img/blumenau.jpg";
+}
+function litoral (){
+    document.getElementById("trocarimg").src="img/litoral.jpg";
+}
+function belem (){
+    document.getElementById("trocarimg").src="img/belem.jpg";
+}
