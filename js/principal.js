6972bb2b31762ae7f97c0828a717d5a4a7ba2e00
Kemoel <kemoel.amorim@gmail.com>
2020-12-07 13:26:59 -0300

    js implementado

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,35 @@
 function salvador(){
+    var titulo = document.querySelector(".titulo")
+    titulo.textContent = "Salvador"
     document.getElementById("trocarimg").src="img/salvador.jpg";
-}
+    var paragrafo = document.querySelector(".texto")
+    paragrafo.textContent = "A cidade nasceu como protagonista do país: em 1549 foi declarada a primeira capital do Brasil. Claro, a localização era estratégica para os europeus – mas não há dúvida de que as belezas naturais também arrebataram os navegantes. Difícil até hoje não se impressionar com a paisagem das praias do norte, como Itapuã, eternizada por Dorival Caymmi, Vinicius de Moraes e Toquinho, ou mesmo da orla central, onde fica a Praia da Barra, famosa por seu farol. Mais que a natureza, o homem fez de Salvador lugar especial, na beleza das construções históricas, museus, gastronomia e hospitalidade"
+    }
 function rio (){
+    var titulo = document.querySelector(".titulo")
+    titulo.textContent = "Rio de Janeiro"
     document.getElementById("trocarimg").src="img/rio.jpg";
+    var paragrafo = document.querySelector(".texto")
+    paragrafo.textContent = "Com inúmeras praias, belíssimas montanhas e o samba e a bossa nova ao fundo, é fácil se apaixonar pelo Rio de Janeiro. Imortalizada em uma canção, a praia de Ipanema continua sendo o lugar perfeito para caminhar, pegar um bronzeado e se exibir. A maior estátua Art déco do mundo, o Cristo Redentor, convida todos a uma visita ao Corcovado. E todos os anos, durante o Carnaval, o Rio oferece uma extravagante celebração com muita festa, música, dança e fantasias."
 }
 function blumenau (){
+    var titulo = document.querySelector(".titulo")
+    titulo.textContent = "Blumenau"
     document.getElementById("trocarimg").src="img/blumenau.jpg";
+    var paragrafo = document.querySelector(".texto")
+    paragrafo.textContent = "A região de Blumenau era habitada por índios Kaigangs, Xoklengs também denominados Botocudos e, mesmo antes da fundação da Colônia Blumenau, já havia famílias estabelecidas na região de Belchior, nas margens do ribeirão Garcia e do rio Itajaí-Açú Em 1850, o filósofo alemão Dr. Hermann Bruno Otto Blumenau obteve do Governo Provincial uma área de terras de duas léguas para estabelecer uma colônia agrícola, com imigrantes europeus. Em 2 de setembro de 1850, dezessete colonos chegaram ao local onde hoje se ergue a cidade de Blumenau. Muitos outros imigrantes atravessavam o Oceano Atlântico em veleiros de companhias particulares. E assim foi crescendo o número de agricultores, povoadores e cultivadores dos lotes, medidos e demarcados ao longo dos rios e ribeirões que banhavam o território da concessão."
 }
 function litoral (){
+    var titulo = document.querySelector(".titulo")
+    titulo.textContent = "Porto de Galinhas"
     document.getElementById("trocarimg").src="img/litoral.jpg";
+    var paragrafo = document.querySelector(".texto")
+    paragrafo.textContent = "A uma hora ou duas ao sul de Recife, Porto de Galinhas foi criado para o turista não tão radical. Um viajante declarou entusiasmado: É o que ele não tem que o torna ótimo: nada de trânsito complicado, nada de vida noturna. Explore as piscinas naturais que se formam no recife, dê comida para os peixes que nadam a sua volta, vá de buggy até a romântica Praia Muro Alto. Ou esqueça tudo isso e durma em uma rede ouvindo e vendo de perto o Atlântico morno, verde e transparente."
 }
 function belem (){
+    var titulo = document.querySelector(".titulo")
+    titulo.textContent = "Belém"
     document.getElementById("trocarimg").src="img/belem.jpg";
+    var paragrafo = document.querySelector(".texto")
+    paragrafo.textContent = "Belém, capital do estado de Pará, é uma cidade portuária e a porta de entrada para a região do Baixo Amazonas do Brasil. Junto à Baía do Guajará, o bairro da Cidade Velha à beira-rio preserva a arquitetura colonial portuguesa, das igrejas e casas com azulejos coloridos a uma fortificação do século XVII conhecida como Forte do Castelo. Ver-o-Peso é um amplo mercado ao ar livre junto à água, onde se vende peixe, fruta e artesanato da Amazónia."
 }
